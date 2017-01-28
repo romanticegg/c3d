a644cf70c3abd4538ffb0fc7f039ef07ef5d7542
zijwei <zijwei@cs.stonybrook.edu>
2017-01-27 20:56:37 -0500

    check_tfrecords

--- check_tf_records.py
+++ check_tf_records.py
@@ -1,9 +1,13 @@
 # check if the tfrecord is well done:
 import tensorflow as tf
 import c3d_input_ucf101
+import tf_utils
+
 flags = tf.app.flags
 flags.DEFINE_string('file_path', '/Users/zijwei/Dev/datasets/UCF-101-g16/train', 'Path to check the files')
 flags.DEFINE_integer('batch_size', 1, 'Batch size[1]')
+flags.DEFINE_integer('gpu_id', None, 'GPU [None]')
+
 FLAGS = flags.FLAGS
 
 
@@ -11,7 +15,9 @@ def main(argv=None):
     with tf.Graph().as_default() as graph:
         # tf_images, tf_lb, tf_filename, tf_sampl_start, d, h, w, c = c3d_input_ucf101.inputs(filepath)
         tf_images, tf_lb, tf_fnames = c3d_input_ucf101.inputs(FLAGS.file_path, isTraining=True)
-        with tf.Session() as sess:
+        config = tf_utils.gpu_config(FLAGS.gpu_id)
+
+        with tf.Session(config=config) as sess:
             sess.run(tf.variables_initializer(tf.global_variables()))
 
             coord = tf.train.Coordinator()
