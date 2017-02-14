8a15544f1c9e71a26c7d6afef8367cb472c919ef
zijwei <zijwei@cs.stonybrook.edu>
2017-02-14 08:57:06 -0500

    bugfix

--- simplenns/cifar10_eval.py
+++ simplenns/cifar10_eval.py
@@ -47,7 +47,7 @@ def eval():
 
         config = tf_utils_inner.gpu_config(FLAGS.gpu_id)
         with tf.Session(config=config) as sess:
-            sess.run(tf.variables_initializer(tf.global_variables()))
+            # sess.run(tf.variables_initializer(tf.global_variables()))
             saver.restore(sess=sess,save_path=FLAGS.model)
             nbatches = int(math.ceil(cifar10_inputs.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL*1.0/FLAGS.batch_size))
 
